6bb65a940353f7a80037f9b2526b9eb71b690adf
Yugo Yagita <2000ygkn0713@gmail.com>
2019-08-09 23:40:38 +0900

    snake_case

--- porker.py
+++ porker.py
@@ -1,31 +1,32 @@
 from random import sample
+from card import Card
 
 suits = ['♠', '♥', '♦', '♣' ]
 
 class Porker:
-  def __init__(self, numberOfPlayers):
-    if(numberOfPlayers > 10):
-      raise Exception("numberOfPlayers should be less than 10.")
+  def __init__(self, number_of_players):
+    if(number_of_players > 10):
+      raise Exception("number_of_players should be less than 10.")
 
     # シャッフルされた52までの数字（カードを表す）を作る
-    shuffledCards = sample(range(52), 52)
+    shuffled_cards = sample(range(52), 52)
 
     # shuffledCardsから5枚づつ取って，各プレイヤーの手札とする
     self.playerCards = list(map(
-      lambda i: shuffledCards[(i * 5):((i + 1) * 5)]
-      , range(numberOfPlayers)))
+      lambda i: shuffled_cards[(i * 5):((i + 1) * 5)]
+      , range(number_of_players)))
 
     # 残りを山札とする
-    self.deck = shuffledCards[5 * numberOfPlayers:]
+    self.deck = shuffled_cards[5 * number_of_players:]
 
-  def draw(self, playerIndex, cardIndex):
+  def draw(self, player_index, card_index):
     if(len(self.deck) == 0):
       raise Exception("there is no remaining deck.")
-    self.playerCards[playerIndex][cardIndex] = self.deck.pop()
+    self.playerCards[player_index][card_index] = self.deck.pop()
   
-  def cardsStr(self, playerIndex):
-    if(len(self.playerCards) < playerIndex):
-      raise Exception("playerIndex is out of range")
+  def cardsStr(self, player_index):
+    if(len(self.playerCards) < player_index):
+      raise Exception("player_index is out of range")
     return ','.join(map(
         lambda card: suits[int(card / 13)] + '-' + str(card % 5 + 1)
-      ,self.playerCards[playerIndex]))
+      ,self.playerCards[player_index]))
